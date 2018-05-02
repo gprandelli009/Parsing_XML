2c5c65cc551e3e49411da60867e2c8aba2e9fc56
Giovanni Prandelli <Xivolg98@gmail.com>
2018-05-02 12:29:42 +0200

    Inizio Progetto

---
+++ src/Agency.java
@@ -0,0 +1,19 @@
+public class Agency {
+	String name="";
+	String surname="";
+	public Agency() {}
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getSurname() {
+		return this.surname;
+	}
+	public void setSurname(String surname) {
+		this.surname=surname;
+}
+}
+
+
---
+++ src/MainApp.java
@@ -0,0 +1,11 @@
+import support.Read;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Read read_xml_Agency=new Read();
+		read_xml_Agency.explore("agency.xml");
+	}
+
+}
